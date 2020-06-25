923c7aaa0adbdbe6da3a2887a68b5489860d35cf
Gerard Altamirano <galtamiranor@outlook.es>
2020-06-25 17:16:09 -0500

    - [x] User can starred a board - [x] Minor fixes

--- src/app.js
+++ src/app.js
@@ -1,54 +1,43 @@
-window.addEventListener('load', () => {
-    // for( let i = 0; i < 12; i++) {
-    //     drawBoard(BoardElement({id: `board-${i}`, title: `BOARD ${i + 1}`, color: 'color-1'}));
-    // }
-    //
-    const removeBoardHandler = e => {
-       const tagName = e.target.localName;
-       const el = tagName === 'img' ? e.target.parentElement.parentElement : e.target.parentElement;
-        console.log(el.dataset.id);
-    }
-    drawBoards();
-    addListeners('.button__star_board', 'click', removeBoardHandler);
-    addColorListeners();
-    createBoardListener();
-    modalListeners();
-});
-
-const state = {
-    lastElementId: 0
+const Id = () => {
+    const lastId = state.lastElementId;
+    state.updateId();
+    return lastId;
 };
 
+const state = {
+    lastElementId: Number(localStorage.getItem('id')) || 0,
+    updateId: (id = null) => {
+        state.lastElementId++;
+        localStorage.setItem('id', String(state.lastElementId))
+    },
+};
 
 const getBoards = () => JSON.parse(localStorage.getItem('boards'));
 const saveBoards  = boards => localStorage.setItem('boards', JSON.stringify(boards));
 
 const saveBoard = board => {
-    console.log(board)
     const localBoards = getBoards();
     if(!localBoards) return saveBoards([board]);
 
     localBoards.push(board);
     saveBoards(localBoards);
-    // TODO: Render new Board
+    clearBoardForm();
 }
-
 
 const getElement = selectors => document.querySelector(selectors);
 const getElements = selectors => document.querySelectorAll(selectors);
 
-const createBoardModal = getElement('#create_board__screen');
+const getCreateBoardModalElement = getElement('#create_board__screen');
 
 const clearBoardForm = () => getElement('#create_board__form__title').value = '';
 const openCreateBoardModal = () => {
     changeCardFormColor('color-1');
     getElement('body').classList.add('disable_body_scroll')
-    createBoardModal.style.display = 'block';
+    getCreateBoardModalElement.style.display = 'block';
 };
 const closeCreateBoardModal = () => {
     getElement('body').classList.remove('disable_body_scroll')
-    clearBoardForm();
-    createBoardModal.style.display = 'none'
+    getCreateBoardModalElement.style.display = 'none'
 };
 
 
@@ -57,11 +46,10 @@ const createBoardHandler = e => {
     const color = target.previousElementSibling.classList.item(1);
     const title = target.previousElementSibling.firstElementChild.firstElementChild.value;
     if(title === '') return;
-    board = Board(color, title);
+    const board = Board(color, title);
     saveBoard(board);
     closeCreateBoardModal();
-    drawBoard(BoardElement(Object.assign(board, { id: state.lastElementId })))
-    state.lastElementId++;
+    drawBoard(board)
 }
 const createBoardListener = () => addListener('#btn-create-board', 'click', createBoardHandler);
 
@@ -86,7 +74,7 @@ const modalListeners = () => {
     screenModalListener();
     ESCListener();
 }
-const Board = (color, title, isStarred = false) => { return {color, title, isStarred} }
+const Board = (color, title, isStarred = false) => { return {id: Id(),color, title, isStarred} }
 
 const changeCardFormColor = color => {
     getElement('.create_board__form').setAttribute('class', `create_board__form ${color}`);
@@ -104,23 +92,77 @@ const addListeners = (selectors, event, handler) => {
     getElements(selectors).forEach(element => addListener(element, event, handler));
 }
 
+const filterStarredBoards = board => board.isStarred;
+const filterNoStarredBoards = board => !board.isStarred
+
+// TODO: Revisar esto y ver si se puede refactorizar
+const drawStarredBoards = () => {
+    const boards = getBoards();
+    if(!boards) return;
+    boards.filter(filterStarredBoards).forEach(drawStarredBoard);
+};
+
+const drawBoard = board => {
+    const boardElement = BoardElement(board);
+    getElement('#boards__tiles').insertBefore(boardElement, getElement('.boards__tiles__create'));
+    starredBoardListeners();
+};
 const drawBoards = () => {
     const boards = getBoards();
-    const boardsLength = boards ? boards.length : 0;
     if(!boards) return;
-    boards.forEach( (board, id) => {
-        drawBoard(
-            BoardElement(Object.assign(board, { id }))
-        )
-    });
-
-    state.lastElementId = boardsLength;
+    boards.filter(filterNoStarredBoards).forEach(drawBoard);
 }
 
-const drawBoard = boardElement => {
-    getElement('#boards__tiles').insertBefore(boardElement, getElement('.boards__tiles__create'));
+
+const drawStarredBoard = board => {
+    const boardElement = StarredBoardElement(board);
+    getElement('#header__starred_boards').append(boardElement);
+    starredBoardListeners();
 }
 
+
+const getButtonsElementFromButtonEvent = e => {
+    const tagName = e.target.localName;
+    return tagName === 'img' ? e.target.parentElement.parentElement : e.target.parentElement;
+}
+
+const closeBoardHandler = e => {
+    const id = getButtonsElementFromButtonEvent(e).dataset.id;
+    console.log(id);
+};
+const closeBoardListeners = () => addListeners('.button__close_board', 'click', closeBoardHandler);
+
+const removeBoard = id => {
+    getElement(`[data-id=board-${id}]`).parentElement.remove();
+}
+
+const unstarBoard = index => {
+    const boards = getBoards();
+    const board = boards[index]
+    board.isStarred = false;
+    removeBoard(board.id);
+    saveBoards(boards);
+    drawBoard(board);
+};
+const starBoard = index => {
+    const boards = getBoards();
+    const board = boards[index]
+    board.isStarred = false;
+    removeBoard(board.id);
+    saveBoards(boards);
+    drawStarredBoard(board);
+}
+const starredBoardHandler = e => {
+    const element = getButtonsElementFromButtonEvent(e);
+    const id = element.dataset.id;
+    const index = getBoards().findIndex(el => el.id == id);
+    if(getButtonsElementFromButtonEvent(e).lastElementChild.classList.contains('starred')) {
+        return unstarBoard(index);
+    }
+
+    starBoard(index);
+};
+const starredBoardListeners = () => addListeners('.button__star_board', 'click', starredBoardHandler);
 
 const BoardElement = ({id, title, color}) => {
     return (
@@ -132,6 +174,19 @@ const BoardElement = ({id, title, color}) => {
     );
 };
 
+const StarredBoardElement = ({id, title, color}) => {
+    const element = stringToHtml(
+        boardTemplate.replace(/:id:/g, id)
+            .replace(/:title:/g, title)
+            .replace(/:color:/, color)
+    );
+
+    const button = element.children[1].firstElementChild.lastElementChild;
+    button.classList.add('starred');
+    button.setAttribute('title', 'Unstar Board');
+    return element;
+};
+
 const stringToHtml = string => {
     const parent = document.createElement('div');
     parent.innerHTML = string;
@@ -140,12 +195,23 @@ const stringToHtml = string => {
 
 // language=HTML
 const boardTemplate = `
-    <div class="boards__tiles__tile :color:"><p class="boards__tiles__tile__title" data-id=":id:">:title:</p>
+    <div class="boards__tiles__tile :color:"><p class="boards__tiles__tile__title" data-id="board-:id:">:title:</p>
         <div class="boards__tiles__tile__hover_content">
             <div class="boards__tile__tile__hover_content__icons" data-id=":id:">
-                <button class="button__star_board"><img src="./img/icons/remove.png" alt=""/></button>
-                <button><img src="./img/icons/white_star.png" alt=""/></button>
+                <button class="button__close_board" title="Close Board"><img src="./img/icons/remove.png" alt="" /></button>
+                <button class="button__star_board" title="Star Board"><img src="./img/icons/white_star.png" alt="" /></button>
             </div>
         </div>
     </div>  `;
 
+
+window.addEventListener('load', () => {
+
+    drawBoards();
+    drawStarredBoards();
+    starredBoardListeners();
+    closeBoardListeners();
+    addColorListeners();
+    createBoardListener();
+    modalListeners();
+});
